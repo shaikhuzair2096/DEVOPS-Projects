c7f0aa38d8ecb8ac81535b8dddb5c036d9f7373d
shaikhuzair2096 <shaikhuzair2096@gmail.com>
2026-05-08 00:14:43 +0530

    Create variable.tf

---
+++ terraform/vpc-terraform/dev-env/variable.tf
@@ -0,0 +1,63 @@
+variable "dev_network_host_project_id" {
+  description = "Project ID of Shared VPC host project"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of Shared VPC network"
+  type        = string
+}
+
+variable "service_projects" {
+  description = "List of service projects attached to Shared VPC"
+  type        = list(string)
+}
+
+variable "default_project_id" {
+  description = "default_project_id"
+  type        = string
+}
+
+variable "default_region" {
+  description = "default_region"
+  type        = string
+}
+
+variable "impersonate_service_account" {
+  description = "impersonate_service_account"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Map of subnet definitions"
+
+  type = map(object({
+    ip_cidr_range = string
+    region        = string
+
+    secondary_ranges = list(object({
+      range_name    = string
+      ip_cidr_range = string
+    }))
+  }))
+}
+
+variable "firewall_rules" {
+  description = "Firewall rules for Shared VPC"
+
+  type = map(object({
+    direction     = string
+    priority      = number
+    source_ranges = optional(list(string))
+    target_tags   = optional(list(string))
+
+    allow = optional(object({
+      protocol = string
+      ports    = optional(list(string))
+    }))
+
+    deny = optional(object({
+      protocol = string
+    }))
+  }))
+}
